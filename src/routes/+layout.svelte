61f5094c46a3a80d1efc47861c940e5ee7bb499f
Peter Gyorfi <apgyorfi@protonmail.com>
2024-03-02 20:45:13 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.css";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<slot />
+<Footer/>
+
+
+
+<!-- header
+hero
+product description
+user reviews
+faq
+conversion-
+footer -->
